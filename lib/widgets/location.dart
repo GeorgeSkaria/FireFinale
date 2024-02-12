02e2c0f2b4144752a5285e2def97b844c485cd24
sanu00007 <saninmohammedn@gmail.com>
2024-02-12 12:33:13 +0530

    location page

--- lib/widgets/location.dart
+++ lib/widgets/location.dart
@@ -23,12 +23,11 @@ class MyMap extends StatelessWidget {
           ),
         ),
       ),
-      body: Image.asset(
-        'assets/images/vector.jpg',
-        fit: BoxFit.fitWidth,
-        // Use BoxFit.fitWidth to fit the image within the width of the screen
-        width: double.infinity,
-      ),
+      body: Image.asset('assets/images/vector.jpg',
+          fit: BoxFit.cover,
+          // Use BoxFit.fitWidth to fit the image within the width of the screen
+          width: double.infinity,
+          height: double.infinity),
       bottomNavigationBar: BottomAppBar(
         color: const Color.fromARGB(255, 252, 252, 252),
         shape: CircularNotchedRectangle(),
